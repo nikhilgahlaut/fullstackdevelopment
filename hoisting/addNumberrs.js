8e8cec0b3d382d6e896e3734ef91f84eec28a41c
Nikhil Gahlaut <40818207+nikhilgahlaut@users.noreply.github.com>
2023-04-13 23:11:34 +0530

    Add files via upload

---
+++ hoisting/addNumberrs.js
@@ -0,0 +1,4 @@
+console.log(addTwo(8,8))
+function addTwo(num1 ,num2){
+    return num1 + num2;
+};
---
+++ hoisting/multiplyNumbers.js
@@ -0,0 +1,4 @@
+console.log(multiplyNumbers(3,2));
+const  multiplyNumbers = function(num1, num2){
+    return num1 * num2
+}
