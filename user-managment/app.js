e34e0cf4bfd4c447cf2a8e82e47a1293eace6c06
Nikhil Gahlaut <40818207+nikhilgahlaut@users.noreply.github.com>
2023-09-24 16:51:28 +0530

    Add files via upload

--- user-managment/app.js
+++ user-managment/app.js
@@ -2,11 +2,18 @@ require('dotenv').config()
 const express = require('express')
 
 const connectToDb = require('./config/db.js')
-const{home} = require('./controllers/userController.js')
+// const{home} = require('./controllers/userController.js')
+const cors = require('cors')
 
 const app = express()
 
+connectToDb()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cors())
 
-app.get('/',home)
+const userRoutes = require('./routes/userRoutes.js')
+
+app.use('/',userRoutes)
 
 module.exports = app
---
+++ user-managment/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const {home,createUser,getUsers,deleteUser,editUser} = require('../controllers/userController.js')
+
+const router = express.Router()
+
+router.get('/',home)
+router.post('/createuser',createUser)
+router.get('/getuser',getUsers)
+router.delete('/deleteuser/:id', deleteUser)
+router.put('/updateuser/:id', editUser)
+
+
+module.exports = router
+
